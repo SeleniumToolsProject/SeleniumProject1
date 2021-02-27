b9754d69975238d873204cefb2f868ec41f69b17
Lenovo <Lenovo@Lenovo-PC>
2021-02-27 23:25:08 +0530

    QA changes in Test.java class

--- QAProj1/src/com/qa/page/Test.java
+++ QAProj1/src/com/qa/page/Test.java
@@ -1,8 +1,14 @@
 package com.qa.page;
 
+import com.qa.base.BaseElement;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		BaseElement base=new BaseElement();
+		
+		System.out.println("Base element object is created");
 		
 		System.out.println("Test class Displayed");
 
