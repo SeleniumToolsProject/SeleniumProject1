e83178e0d00bf0fd7988bd103537edb1ccd639cf
Lenovo <Lenovo@Lenovo-PC>
2021-02-27 23:50:28 +0530

    QA changes in m1() method of HomePage class

--- QAProj1/src/com/qa/page/HomePage.java
+++ QAProj1/src/com/qa/page/HomePage.java
@@ -3,7 +3,7 @@ package com.qa.page;
 public class HomePage {
 	
 	public void m1() {
-		System.out.println("m1 method created");
+		System.out.println("m1 method created :12345");
 	}
 
 	public static void main(String[] args) {
