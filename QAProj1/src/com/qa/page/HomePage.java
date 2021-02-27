526b50f10659a2e561d96f52ea54142e058137cb
Lenovo <Lenovo@Lenovo-PC>
2021-02-27 23:51:35 +0530

    TL change in m1() method

--- QAProj1/src/com/qa/page/HomePage.java
+++ QAProj1/src/com/qa/page/HomePage.java
@@ -3,7 +3,7 @@ package com.qa.page;
 public class HomePage {
 	
 	public void m1() {
-		System.out.println("m1 method created");
+		System.out.println("m1 method created........abcde");
 	}
 
 	public static void main(String[] args) {
