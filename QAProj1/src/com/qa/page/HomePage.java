e668423c09cbf9fa336a722e2442902673019a1c
Lenovo <Lenovo@Lenovo-PC>
2021-02-28 13:32:42 +0530

    ded

--- QAProj1/src/com/qa/page/HomePage.java
+++ QAProj1/src/com/qa/page/HomePage.java
@@ -8,6 +8,19 @@ public class HomePage {
 		System.out.println("m1 method is modified");
 
 	}
+	
+	public void m2() {
+		System.out.println("m2 method is displayed");
+	}
+	
+	public void m3() {
+		System.out.println("m3 method is displayed");
+	}
+	public void m4() {
+		
+		System.out.println("m4 is dispalyed");
+	}
+	
 
 	public static void main(String[] args) {
 		
