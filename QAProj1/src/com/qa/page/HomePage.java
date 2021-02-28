a60303d7c0608e613c186e354d0d9c8dbf50813e
Lenovo <Lenovo@Lenovo-PC>
2021-02-28 12:20:48 +0530

    QA modified m1 method

--- QAProj1/src/com/qa/page/HomePage.java
+++ QAProj1/src/com/qa/page/HomePage.java
@@ -4,6 +4,7 @@ public class HomePage {
 	
 	public void m1() {
 		System.out.println("m1 method created :12345");
+		System.out.println("m1 method is modified");
 	}
 
 	public static void main(String[] args) {
